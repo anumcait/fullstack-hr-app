6bfe66478e1f63ae9c865bd4685f254f8c078230
anumcait <anumcait@gmail.com>
2025-06-21 11:25:02 +0530

    vite config changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 4173,
+    strictPort: true,
+    allowedHosts: ['fullstack-hr-app-frontend.onrender.com'],
+  },
   server: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 5173
-  }
+    port: parseInt(process.env.PORT) || 5173,
+  },
 });
