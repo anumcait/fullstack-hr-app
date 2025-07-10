3025c4ed647025bb9d374cd43765382907b75ad5
anumcait <anumcait@gmail.com>
2025-07-10 15:55:35 +0530

    New changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,18 +3,32 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 4173,
-    strictPort: true,
-    allowedHosts: ['fullstack-hr-app-frontend.onrender.com'],
-  },
   server: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 5173,
-      strictPort: true,
-  watch: {
-    usePolling: true,
-  },
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
   },
 });
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   preview: {
+//     host: '0.0.0.0',
+//     port: parseInt(process.env.PORT) || 4173,
+//     strictPort: true,
+//     allowedHosts: ['fullstack-hr-app-frontend.onrender.com'],
+//   },
+//   server: {
+//     host: '0.0.0.0',
+//     port: parseInt(process.env.PORT) || 5173,
+//       strictPort: true,
+//   watch: {
+//     usePolling: true,
+//   },
+//   },
+// });
