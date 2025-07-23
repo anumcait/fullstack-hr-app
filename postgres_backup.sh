51a35b906ec189c3e53471a7315442a4eccb0f1d
anumcait <anumcait@gmail.com>
2025-07-23 17:00:19 +0530

    New2507251700

--- postgres_backup.sh
+++ postgres_backup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Set timestamp
-TIMESTAMP=$(date +%Y%m%d%H%M%S)
+# # Set timestamp
+# TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
-# 1. Create backup file inside container
-docker exec -u postgres hr_postgres \
-  pg_dump -Fc -d hrdb -f /pg_backup/hrdb_${TIMESTAMP}.dump
+# # 1. Create backup file inside container
+# docker exec -u postgres hr_postgres \
+#   pg_dump -Fc -d hrdb -f /pg_backup/hrdb_${TIMESTAMP}.dump
 
-# 2. Copy from container to host
-docker cp hr_postgres:/pg_backup ./pg_backup
+# # 2. Copy from container to host
+# docker cp hr_postgres:/pg_backup ./pg_backup
 
-echo "✅ Backup completed: ./pg_backup/hrdb_${TIMESTAMP}.dump"
+# echo "✅ Backup completed: ./pg_backup/hrdb_${TIMESTAMP}.dump"
+
+docker exec hr_postgres chown -R postgres:postgres pg_backup && docker exec -u postgres hr_postgres bash -c 'pg_dump -Fc -d hrdb -f /pg_backup/hrdb_temp.dump' && docker cp hr_postgres:/pg_backup/hrump' && docker cp hr_postgres:/pg_backup/hrdb_temp.dump ./pg_restore/hrdb.backup
+Successfully copied 24.1kB to D:\Fullstack-hr-app\pg_restore\hrdb.backup
