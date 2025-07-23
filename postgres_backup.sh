959d6fdf08379771ac135b83aec7907259de8cea
anumcait <anumcait@gmail.com>
2025-07-23 17:19:38 +0530

    New2507251719

--- postgres_backup.sh
+++ postgres_backup.sh
@@ -13,4 +13,3 @@
 # echo "✅ Backup completed: ./pg_backup/hrdb_${TIMESTAMP}.dump"
 
 docker exec hr_postgres chown -R postgres:postgres pg_backup && docker exec -u postgres hr_postgres bash -c 'pg_dump -Fc -d hrdb -f /pg_backup/hrdb_temp.dump' && docker cp hr_postgres:/pg_backup/hrump' && docker cp hr_postgres:/pg_backup/hrdb_temp.dump ./pg_restore/hrdb.backup
-Successfully copied 24.1kB to D:\Fullstack-hr-app\pg_restore\hrdb.backup
